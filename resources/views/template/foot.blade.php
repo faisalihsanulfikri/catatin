a75c97d40038e93da9e63decc6b30f46edb17dc9
faisal.ihsanulfikri <faisalihsanulfikri@gmail.com>
2023-09-05 05:50:22 +0700

    fix

--- resources/views/template/foot.blade.php
+++ resources/views/template/foot.blade.php
@@ -27,7 +27,7 @@
 <script src="{{asset('plugins/input-mask/jquery.inputmask.bundle.min.js')}}"></script>
 
 <script src="{{asset('plugins/flatpickr/flatpickr.js')}}"></script>
-<script src="{{asset('plugins/flatpickr/custom-flatpickr.js')}}"></script>
+<!-- <script src="{{asset('plugins/flatpickr/custom-flatpickr.js')}}"></script> -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment-with-locales.min.js" integrity="sha512-LGXaggshOkD/at6PFNcp2V2unf9LzFq6LE+sChH7ceMTDP0g2kn6Vxwgg7wkPP7AAtX+lmPqPdxB47A0Nz0cMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 <script>
